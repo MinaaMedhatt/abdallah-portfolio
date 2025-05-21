ca61109e28a2d360f2fe8fc168c16971c3581525
MinaaMedhatt <166822133+MinaaMedhatt@users.noreply.github.com>
2025-05-21 23:37:16 +0300

    Update config.js

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -39,8 +39,8 @@ const font = {
 
 const style = {
   theme: "dark", // theme is not necessary when using ThemeProvider (default)
-  neutral: "sand", // sand | gray | slate
-  brand: "cyan", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  neutral: "slate", // sand | gray | slate
+  brand: "magenta", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
   accent: "indigo", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
   solid: "contrast", // color | contrast
   solidStyle: "flat", // flat | plastic
